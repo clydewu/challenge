4c927e54c0dcc792c268954b7a543fe083a1eea3
Clyde Wu <claudwu@gmail.com>
2018-07-22 16:30:17 +0800

    [#xxxxx] Refactory with pandas style instead of pyton in-built list/dict

--- masky.py
+++ masky.py
@@ -2,7 +2,8 @@
 '''
 '''
 import os
-from datetime import datetime
+
+import ipdb
 
 from flask import Flask
 
@@ -27,55 +28,28 @@ def load(file_path, turncate):
     @param file_name: The string file path of CSV file
     @param turncate: Whether clear up collection before insertion
     '''
-    import ipdb; ipdb.set_trace()
-    df, modify_dt = _load_csv_to_df(file_path)
-    collection = _get_collection('masky', 'fruits')
-    add_dict = {'datetime': modify_dt.strftime('%Y-%m-%d %H:%M:%S')}
-    input_dict = _df_2_dict(df)
-    updated_dict = _add_dict_field(input_dict, add_dict)
-    bulk_operations = _gen_bulk_operations(updated_dict, turncate)
+    app.logger.info('Execute load command, file_path: {}, turncate: {}'.format(file_path, turncate))
+    collection = MongoClient()['masky']['fruits']
+    mtime = os.stat(file_path).st_mtime
+    df = pandas.read_csv(file_path, header=None, names=['id', 'name', 'price', 'amount'])
+    app.logger.info('Load CSV file ok, file_path: {}, mtime: {}'.format(file_path, mtime))
+
+    app.logger.info('Add additional time field, timestamp: {}'.format(mtime))
+    df['mtime'] = mtime
+    df['mtime'] = pandas.to_datetime(df['mtime'].astype(int), unit='s')
+
+    app.logger.info('Prepare write into DB, count: {}, turncate: {}'.format(df.shape[0], turncate))
+    bulk_operations = _gen_bulk_operations(df, turncate)
     collection.bulk_write(bulk_operations)
 
 
-def _load_csv_to_df(file_path):
-    app.logger.info('Load {} ...'.format(file_path))
-    df = pandas.read_csv(file_path, header=None, names=['id', 'name', 'price', 'amount'])
-    modify_dt = datetime.fromtimestamp(os.stat(file_path).st_mtime)
-    df.info()
-    app.logger.info('Load CSV file ok, file_path: {}, modify time: {}.'.format(file_path, modify_dt))
-    return df, modify_dt
-
-
-def _get_collection(db_name, collection_name):
-    return MongoClient()[db_name][collection_name]
-
-
-def _df_2_dict(data_frame):
+def _gen_bulk_operations(date_frame, turncate=False):
     '''
-    @param data_frame: Input DataFrame
-    @param additional_dict: The additional dictionary which will be added into each row
+    @param date_frame: The dictionary will be insert into DB
     @return: A list of BulkOperation subclass
     '''
-    return [r[1].to_dict() for r in data_frame.iterrows()]
-
-
-def _add_dict_field(input_dict, additional_dict):
-    # -- Due to additional_dict is not in the scropt of map() or lambda, use for-loop instead of
-    for d in input_dict:
-        d.update(additional_dict)
-    return input_dict
-
-
-def _modify_dict_value(input_dict, action):
-    return map(action, input_dict)
-
-
-def _gen_bulk_operations(input_dict, turncate=False):
-    '''
-    @param input_dict: The dictionary will be insert into DB
-    @return: A list of BulkOperation subclass
-    '''
-    return ([DeleteMany({})] if turncate else []) + [InsertOne(d) for d in input_dict]
+    return ([DeleteMany({})] if turncate else []) + \
+        [InsertOne(r[1].to_dict()) for r in date_frame.iterrows()]
 
 
 @app.cli.command('alter', help='Alter a csv file and save to mongoDB')
